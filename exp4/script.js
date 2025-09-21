d37ad062aa6d09cd9f4cf0bb12c092570430da5c
Parveen kaswan <23bct10014@gmail.com>
2025-09-21 17:48:00 +0530

    Add files via upload

---
+++ exp4/script.js
@@ -0,0 +1,10 @@
+function updateCount(textareaId, countId) {
+  var text = document.getElementById(textareaId).value;
+  document.getElementById(countId).innerText = text.length;
+}
+
+document.getElementById("text1").addEventListener("input", function() {
+  updateCount("text1", "count1");
+});
+
+
